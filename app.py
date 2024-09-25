0d2917baf2783c60611f313aed358a9263b13028
Nandu756G <107916708+Nandu756G@users.noreply.github.com>
2024-09-26 01:08:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if 'search_keyword' not in st.session_state:
 if 'search_performed' not in st.session_state:
     st.session_state.search_performed = False
 
-st.title("Bilingual OCR Insight: Text Extraction and Search")
+st.title("IndiOCR")
 
 # Create an instance of the OCR model
 @st.cache_resource
